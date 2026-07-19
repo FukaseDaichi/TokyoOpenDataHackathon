2d61d9dd9366c8202aa3de34a1129e26741fe9ae
fukase <pecoteach@yahoo.co.jp>
2026-07-19 19:11:20 +0900

    Revert "perf: three.jsを共有チャンクthree-vendorへ分離し二重バンドルを解消"
    This reverts commit 9f027c496263f3db2b2264a028ff0a02fbdf3c49.

--- next.config.ts
+++ next.config.ts
@@ -10,23 +10,6 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
-  webpack: (config, { isServer }) => {
-    // three.js は HeroCanvas（トップ）と WardMap3D（区詳細）の2つの動的チャンクへ
-    // 各384KB複製されるため、共有vendorチャンクへ強制分離して1回のDL/キャッシュにする
-    if (!isServer && config.optimization?.splitChunks) {
-      config.optimization.splitChunks.cacheGroups = {
-        ...config.optimization.splitChunks.cacheGroups,
-        three: {
-          test: /[\\/]node_modules[\\/](three|@react-three)[\\/]/,
-          name: 'three-vendor',
-          chunks: 'async',
-          priority: 40,
-          reuseExistingChunk: true,
-        },
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
